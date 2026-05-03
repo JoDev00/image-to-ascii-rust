1370e2d70e8ae769ff4d15a757b099958fb790b1
JoDev00 <227642771+JoDev00@users.noreply.github.com>
2026-05-03 01:20:36 +0100

    Conversion to command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
+use clap::Parser;
 use image::GenericImageView;
 use std::str;
 
-const ASCII_BRIGHTNESS_VALUES: &str = "@@#S%?*+;:,";
-const SIZE: (u32, u32) = (64, 64);
-
-fn get_ascii_character(avg: u16, alpha: u8) -> char {
-    if alpha == 0 {
-        return ' ';
-    }
-
-    let index = (avg as usize) / (255 / ASCII_BRIGHTNESS_VALUES.len());
-    return ASCII_BRIGHTNESS_VALUES.chars().nth(index).unwrap_or(' ');
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    img_name: String,
+    width: u32,
+    height: u32,
 }
 
+const ASCII_BRIGHTNESS_VALUES: &str = "@@#S%?*+;:,";
+
 fn main() {
-    let img_name = "smiley.png";
-    let img = image::open(format!("src/images/{}", img_name))
+    let args = Args::parse();
+
+    let img = image::open(format!("src/images/{}", args.img_name))
         .unwrap()
-        .resize(SIZE.0, SIZE.1, image::imageops::FilterType::Triangle);
+        .resize(
+            args.width,
+            args.height,
+            image::imageops::FilterType::Triangle,
+        );
     println!("dimensions {:?}", img.dimensions());
 
     for pixel in img.pixels() {
@@ -30,4 +34,13 @@ fn main() {
         }
         print!(" {}", char);
     }
+}
+
+fn get_ascii_character(avg: u16, alpha: u8) -> char {
+    if alpha == 0 {
+        return ' ';
+    }
+
+    let index = (avg as usize) / (255 / ASCII_BRIGHTNESS_VALUES.len());
+    return ASCII_BRIGHTNESS_VALUES.chars().nth(index).unwrap_or(' ');
 }
