07b095c17e861babb7da154113c6544efa9d7017
JoDev00 <227642771+JoDev00@users.noreply.github.com>
2026-05-03 18:29:30 +0100

    Basic frame to ascii working

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use opencv::highgui;
 use opencv::prelude::*;
 use opencv::videoio;
+use std::io::{self, Write};
+
+const ASCII_BRIGHTNESS_VALUES: &str = "@@#S%?*+;:,";
 
 // https://medium.com/@ekfqlwcjswl/using-opencv-in-rust-videocapture-d9e817f8e97c was a very helpful resource for this
 fn main() -> opencv::Result<()> {
@@ -15,10 +18,45 @@ fn main() -> opencv::Result<()> {
         cap.read(&mut frame)?;
         highgui::imshow("frame", &frame)?;
 
-        if highgui::wait_key(10)? == 27 {
+        print!("\x1B[H");
+        print!("{}", frame_to_ascii(&frame)?);
+        io::stdout().flush().unwrap();
+
+        if highgui::wait_key(1)? == 27 {
             break;
         }
     }
 
-    Ok(())
+    return Ok(());
+}
+
+fn frame_to_ascii(frame: &Mat) -> opencv::Result<String> {
+    let mut ascii = String::new();
+
+    for y in 0..frame.rows() {
+        for x in 0..frame.cols() {
+            let pixel = frame.at_2d::<opencv::core::Vec3b>(y, x)?;
+
+            let rgba = pixel.0;
+            let avg = (rgba[0] as u16 + rgba[1] as u16 + rgba[2] as u16) / 3;
+            let char = get_ascii_character(avg, 255);
+            ascii.push(' ');
+            ascii.push(char);
+        }
+        ascii.push('\n');
+    }
+    return Ok(ascii);
+}
+
+fn get_ascii_character(avg: u16, alpha: u8) -> char {
+    if alpha == 0 {
+        return ' ';
+    }
+
+    let index = (avg as usize) / (255 / ASCII_BRIGHTNESS_VALUES.len());
+    return ASCII_BRIGHTNESS_VALUES.chars().nth(index).unwrap_or(' ');
+}
+
+fn get_colored_char(ch: char, r: u8, g: u8, b: u8) -> String {
+    return format!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, ch);
 }
