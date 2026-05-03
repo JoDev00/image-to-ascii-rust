085c1ef6fa12f9f0206b0c46e571ec66ac14d441
JoDev00 <227642771+JoDev00@users.noreply.github.com>
2026-05-03 01:40:14 +0100

    Color now supported!

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn main() {
         if pixel.0 % img.width() == 0 {
             println!();
         }
-        print!(" {}", char);
+        print!(" {}", get_colored_char(char, rgba[0], rgba[1], rgba[2]));
     }
 }
 
@@ -43,4 +43,8 @@ fn get_ascii_character(avg: u16, alpha: u8) -> char {
 
     let index = (avg as usize) / (255 / ASCII_BRIGHTNESS_VALUES.len());
     return ASCII_BRIGHTNESS_VALUES.chars().nth(index).unwrap_or(' ');
+}
+
+fn get_colored_char(ch: char, r: u8, g: u8, b: u8) -> String {
+    return format!("\x1b[38;2;{};{};{}m{}\x1b[0m", r, g, b, ch);
 }
